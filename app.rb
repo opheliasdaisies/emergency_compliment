b605a5e5b10a8bf8067a75a79b88c4850c99ea19
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 15:50:50 -0500

    add logic for compliments to app.rb and set up config.ru file

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require "bundler"
 Bundler.require
+require "./lib/compliment.rb"
 
-module Compliment
+module Compliments
   class App < Sinatra::Application
 
+    compliments=["message1", "message2", "message3"]
+
     get "/compliments" do
-      
+      @compliment = Compliment.new(compliments.sample)
+
+      erb :compliment
     end
 
   end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require "./app"
+
+run Compliments::App.new
