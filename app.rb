3df644a06b5e8dd7952df3e0d3ba6fc45e405120
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 16:38:05 -0500

    add scraped compliments to cycle through

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
 require "bundler"
 Bundler.require
-require "./lib/compliment.rb"
+require "./lib/compliment"
+require "./lib/scraper"
 
 module Compliments
   class App < Sinatra::Application
 
-    compliments=["message1", "message2", "message3"]
+    compliments = Scraper.new("http://www.pinterest.com/myrlie93/365-quotes-sayings-and-compliments-from-my-best-fr/").get_compliments
 
     get "/compliments" do
       @compliment = Compliment.new(compliments.sample)
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,5 @@
-require 'nokogiri'
-require 'open-uri'
+require "open-uri"
+require "nokogiri"
 
 class Scraper
   attr_reader :html
