2c015f2c9b60a2cda52dd6a45816de1edf5d0972
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 16:48:13 -0500

    scrape compliments and store in a yaml file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 require "nokogiri"
+require "yaml"
 
 class Scraper
   attr_reader :html
@@ -9,9 +10,16 @@ class Scraper
   end
 
   def get_compliments
-    complements = html.search('.pinDescription').map do |item|
+    compliments = html.search('.pinDescription').map do |item|
       item.text.gsub(/\d+\.\s/, "")
+    end
+    
+    File.open("compliments.yaml", "w") do |file|
+      file.puts YAML::dump(compliments)
     end
   end
 
 end
+
+test = Scraper.new("http://www.pinterest.com/myrlie93/365-quotes-sayings-and-compliments-from-my-best-fr/")
+test.get_compliments
