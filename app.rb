5ec9981b363d62b7252cf11c78f89e72a6a53aa5
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 17:00:41 -0500

    add compliment phrases from yaml file instead of scraping each time

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require "bundler"
 Bundler.require
 require "./lib/compliment"
-require "./lib/scraper"
+require "yaml"
 
 module Compliments
   class App < Sinatra::Application
 
-    compliments = Scraper.new("http://www.pinterest.com/myrlie93/365-quotes-sayings-and-compliments-from-my-best-fr/").get_compliments
+    compliments = YAML.load(File.read("./compliments.yaml"))
 
     get "/compliments" do
       @compliment = Compliment.new(compliments.sample)
