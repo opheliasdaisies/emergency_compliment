7d975b4317c434420d388d6b71ef0ccba8fdc524
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 16:30:50 -0500

    add scraper class

---
+++ lib/scraper.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper
+  attr_reader :html
+
+  def initialize(url)
+    @html = Nokogiri::HTML(open(url))
+  end
+
+  def get_compliments
+    complements = html.search('.pinDescription').map do |item|
+      item.text.gsub(/\d+\.\s/, "")
+    end
+  end
+
+end
