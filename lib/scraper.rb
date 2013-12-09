5c6c0217e3eaa2cf6556e8dde8553067dd55c637
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 17:41:24 -0500

    add image scraper and scrape images to be stored locally

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,21 @@ class Scraper
     end
   end
 
+  def get_images
+    compliments = html.search(".alignnone").map do |item|
+      uri = item['src']
+      File.open(File.basename(uri),'wb'){ |f| f.write(open(uri).read)}
+    end
+    
+    # File.open("compliments.yaml", "w") do |file|
+      # file.puts YAML::dump(compliments)
+    # end
+  end
+
 end
 
-test = Scraper.new("http://www.pinterest.com/myrlie93/365-quotes-sayings-and-compliments-from-my-best-fr/")
-test.get_compliments
+# test = Scraper.new("http://www.pinterest.com/myrlie93/365-quotes-sayings-and-compliments-from-my-best-fr/")
+# test.get_compliments
+
+test = Scraper.new("http://www.demilked.com/men-and-cats-similar-poses/")
+p test.get_images
